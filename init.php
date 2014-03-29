5d947ddb4360a56c84bb611a7e6f34bda3da5cfa
Luke Rollans <hello@lukerollans.me>
2014-03-29 16:12:01 +0800

    Added init and Braintree core

---
+++ init.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Gravity Forms Braintree
+Plugin URI: http://plugify.io/plugin/gravity-forms-braintree
+Description: Allows you to accept credit card payments using Gravity Forms and Braintree Payments
+Author: Luke Rollans
+Version: 0.1
+Author URI: http://www.lukerollans.me
+*/
+
+// Ensure WordPress has been bootstrapped
+if( !defined( 'ABSPATH' ) )
+	exit;
+
+$path = trailingslashit( dirname( __FILE__ ) );
+
+?>
