f0094ced3ea7d5b48a3022dc9e2df55c5a91c4fc
kcppdevelopers <kcppdevelopers@gmail.com>
2022-01-21 16:12:52 +0530

    ACH Form Error, GFB-42

--- assets/js/angelleye-braintree-ach-cc.js
+++ assets/js/angelleye-braintree-ach-cc.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function ($) {
-    if($('.gform_payment_method_options').length){
+    if ($('.gform_payment_method_options').length) {
         var payment_methods = {};
         $('.gform_payment_method_options input[type=radio]').each(function () {
             var targetdiv = $(this).attr('targetdiv');
@@ -7,19 +7,19 @@ jQuery(document).ready(function ($) {
             payment_methods[value] = targetdiv;
         });
 
-        $('.gform_payment_method_options').on('click','input[type=radio]', function () {
+        $('.gform_payment_method_options').on('click', 'input[type=radio]', function () {
             var selectedradio = $(this).val();
             for (var i in payment_methods)
-                if(i!==selectedradio)
-                    $(this).closest('form').find('#'+payment_methods[i]).slideUp()
+                if (i !== selectedradio)
+                    $(this).closest('form').find('#' + payment_methods[i]).slideUp()
 
             var targetdiv = $(this).attr('targetdiv');
-            $(this).closest('form').find('#'+targetdiv).slideDown();
+            $(this).closest('form').find('#' + targetdiv).slideDown();
         });
 
         var selectedradio = $('.gform_payment_method_options input[type=radio]:checked').val();
 
-        switch(selectedradio){
+        switch (selectedradio) {
             case 'braintree_ach':
                 $('.gform_payment_method_options input[value=braintree_ach]').trigger('click');
                 break;
@@ -33,16 +33,17 @@ jQuery(document).ready(function ($) {
 
 
     $('.custom_ach_form_submit_btn').click(function (e) {
+        window[ 'gf_submitting_' + $("input[name='gform_submit']").val() ] = true;
+        $('#gform_ajax_spinner_' + $("input[name='gform_submit']").val()).remove();
         e.preventDefault();
-
         var curlabel = $(this).html();
         var form = $(this).closest('form');
 
         var selectedradio = form.find('.gform_payment_method_options input[type=radio]:checked').val();
 
         var check_if_ach_form = form.find('.ginput_ach_form_container');
-        if(check_if_ach_form.length && (selectedradio === 'braintree_ach' || check_if_ach_form.closest('.gfield').css('display')!=='none')){
-            if(form.find('.ginput_container_address').length==0){
+        if (check_if_ach_form.length && (selectedradio === 'braintree_ach' || check_if_ach_form.closest('.gfield').css('display') !== 'none')) {
+            if (form.find('.ginput_container_address').length == 0) {
                 alert('ACH payment requires billing address fields, so please include Billing Address field in your Gravity form.');
                 return;
             }
@@ -56,67 +57,71 @@ jQuery(document).ready(function ($) {
             var streetAddress = form.find('.ginput_container_address .address_line_1 input[type=text]').val();
             var extendedAddress = form.find('.ginput_container_address .address_line_2 input[type=text]').val();
             var locality = form.find('.ginput_container_address .address_city input[type=text]').val();
-            var region = form.find('.ginput_container_address .address_state input[type=text]').val();
+            var region = form.find('.ginput_container_address .address_state input[type=text], .ginput_container_address .address_state select').val();
             var postalCode = form.find('.ginput_container_address .address_zip input[type=text]').val();
 
+            if (region.length > 2) {
+                region = stateNameToAbbreviation(region);
+            }
+
             var address_validation_errors = [];
-            if(streetAddress==''){
+            if (streetAddress == '') {
                 address_validation_errors.push('Please enter a street address.');
             }
 
-            if(locality==''){
+            if (locality == '') {
                 address_validation_errors.push('Please enter your city.');
             }
 
-            if(region==''){
+            if (region == '') {
                 address_validation_errors.push('Please enter your state.');
             }
 
-            if(postalCode==''){
+            if (postalCode == '') {
                 address_validation_errors.push('Please enter your postal code.');
             }
 
-            if(address_validation_errors.length){
+            if (address_validation_errors.length) {
                 alert(address_validation_errors.join('\n'));
                 return;
             }
 
             var achform_validation_errors = [];
-            if(routing_number=='' || isNaN(routing_number) || account_number=='' || isNaN(account_number)){
+            if (routing_number == '' || isNaN(routing_number) || account_number == '' || isNaN(account_number)) {
                 achform_validation_errors.push('Please enter a valid routing and account number.')
             }
 
-            if(account_type==''){
+            if (account_type == '') {
                 achform_validation_errors.push('Please select your account type.')
             }
 
-            if(account_holdername==''){
+            if (account_holdername == '') {
                 achform_validation_errors.push('Please enter the account holder name.');
-            }else {
+            } else {
                 var account_holder_namebreak = account_holdername.split(' ');
                 if (account_type == 'S' && account_holder_namebreak.length < 2) {
                     achform_validation_errors.push('Please enter the account holder first and last name.');
                 }
             }
 
-            if(account_number !== account_number_verification){
+            if (account_number !== account_number_verification) {
                 achform_validation_errors.push('Account Number and Account Number Verification field should be same.');
             }
 
-            if(achform_validation_errors.length){
+            if (achform_validation_errors.length) {
                 alert(achform_validation_errors.join('\n'));
                 return;
             }
 
             var submitbtn = $(this);
-            submitbtn.attr('disabled', true).html('<span>Please wait...</span>').css('opacity', '0.4');
+            //   submitbtn.attr('disabled', true).html('<span>Please wait...</span>').css('opacity', '0.4');
 
             braintree.client.create({
                 authorization: angelleye_gravity_form_braintree_ach_handler_strings.ach_bt_token
             }, function (clientErr, clientInstance) {
                 if (clientErr) {
                     alert('There was an error creating the Client, Please check your Braintree Settings.');
-                    console.error('clientErr',clientErr);
+                    console.error('clientErr', clientErr);
                     return;
                 }
 
@@ -127,7 +132,7 @@ jQuery(document).ready(function ($) {
                     if (err) {
                         alert('We are unable to validate your system, please try again.');
                         resetButtonLoading(submitbtn, curlabel);
-                        console.error('dataCollectorError',err);
+                        console.error('dataCollectorError', err);
                         return;
                     }
 
@@ -139,7 +144,7 @@ jQuery(document).ready(function ($) {
                         if (usBankAccountErr) {
                             alert('There was an error initiating the bank request. Please try again.');
                             resetButtonLoading(submitbtn, curlabel);
-                            console.error('usBankAccountErr',usBankAccountErr);
+                            console.error('usBankAccountErr', usBankAccountErr);
                             return;
                         }
 
@@ -184,18 +189,83 @@ jQuery(document).ready(function ($) {
 
                             form.append("<input type='hidden' name='ach_device_corelation' value='" + deviceData + "' />");
                             form.append('<input type="hidden" name="ach_token" value="' + tokenizedPayload.nonce + '" />');
+
+
                             form.submit();
                         });
                     });
                 });
             });
 
-        }else {
+        } else {
             form.submit();
         }
     });
 });
 
+function stateNameToAbbreviation(name) {
+    let states = {
+        "Alabama": "AL",
+        "Alaska": "AK",
+        "Arizona": "AZ",
+        "Arkansas": "AR",
+        "California": "CA",
+        "Colorado": "CO",
+        "Connecticut": "CT",
+        "Delaware": "DE",
+        "District of Columbia": "DC",
+        "Florida": "FL",
+        "Georgia": "GA",
+        "Hawaii": "HI",
+        "Idaho": "ID",
+        "Illinois": "IL",
+        "Indiana": "IN",
+        "Iowa": "IA",
+        "Kansas": "KS",
+        "Kentucky": "KY",
+        "Louisiana": "LA",
+        "Maine": "ME",
+        "Maryland": "MD",
+        "Massachusetts": "MA",
+        "Michigan": "MI",
+        "Minnesota": "MN",
+        "Mississippi": "MS",
+        "Missouri": "MO",
+        "Montana": "MT",
+        "Nebraska": "NE",
+        "Nevada": "NV",
+        "New Hampshire": "NH",
+        "New Jersey": "NJ",
+        "New Mexico": "NM",
+        "New York": "NY",
+        "North Carolina": "NC",
+        "North Dakota": "ND",
+        "Ohio": "OH",
+        "Oklahoma": "OK",
+        "Oregon": "OR",
+        "Pennsylvania": "PA",
+        "Rhode Island": "RI",
+        "South Carolina": "SC",
+        "South Dakota": "SD",
+        "Tennessee": "TN",
+        "Texas": "TX",
+        "Utah": "UT",
+        "Vermont": "VT",
+        "Virginia": "VA",
+        "Washington": "WA",
+        "West Virginia": "WV",
+        "Wisconsin": "WI",
+        "Wyoming": "WY",
+        "Armed Forces Americas": "AA",
+        "Armed Forces Europe": "AE",
+        "Armed Forces Pacific": "AP"
+    }
+    if (states[name] !== null) {
+        return states[name];
+    }
+    return name;
+}
+
 function resetButtonLoading(submitbtn, curlabel) {
-    submitbtn.attr('disabled', false).html(curlabel).css('opacity', '1');
+    //  submitbtn.attr('disabled', false).html(curlabel).css('opacity', '1');
 }
